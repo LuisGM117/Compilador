a2a522d14e36baf240010a2e61c0ef3ff19f288c
Luis Angel Garcia Mendez <luis_7gm@outlook.com>
2021-11-23 13:56:45 -0600

    Conditionals now are working

--- BasicCompiler.py
+++ BasicCompiler.py
@@ -30,7 +30,7 @@ TOKEN_LTE           = 'LTE' # ≤
 TOKEN_GTE           = 'GTE' # ≥ 
  
 
-KEYWORDS = ['VAR', 'AND', 'OR', 'IF', 'ELIF', 'ELSE'] 
+KEYWORDS = ['VAR', 'AND', 'OR', 'IF', 'THEN', 'ELIF', 'ELSE'] 
 
 
 
@@ -317,6 +317,13 @@ class BinaryOpNode:
         return f'({self.left_node}, {self.op_tok}, {self.right_node})'
     
 
+class IfNode:
+    def __init__(self, cases, else_case):
+        self.cases = cases
+        self.else_case = else_case
+        self.pos_start = self.cases[0][0].pos_start
+        self.pos_end = (self.else_case or self.cases[len(self.cases)-1][0]).pos_end
+
 
 
 
@@ -376,7 +383,7 @@ class Parser:
                  "Expected ')' "))
 
         elif tok.matches(TOKEN_KEYWORD, 'IF'):
-            if_expr = res.register(self.if_expression())
+            if_expr = res.register(self.if_expr())
             if res.error: return res
             return res.success(if_expr)
         
@@ -401,7 +408,7 @@ class Parser:
 
         
 
-    def if_expression(self):
+    def if_expr(self):
         res = ParseResult()
         cases  = []
         else_case = None
@@ -409,16 +416,23 @@ class Parser:
         if not self.current_tok.matches(TOKEN_KEYWORD, 'IF'):
             return res.failure(CompilerSyntaxError(self.current_tok.pos_start, self.current_tok.pos_end,
             "Expected 'IF' conditional"))
+
         res.register_advancement()
         self.advance()
 
         condition = res.register(self.expression())
         if res.error: return res
 
+
+        if not self.current_tok.matches(TOKEN_KEYWORD, 'THEN'):
+            return res.failure(CompilerSyntaxError(self.current_tok.pos_start, self.current_tok.pos_end,
+                "Expected 'THEN' "))
+        res.register_advancement()
+        self.advance()
+
         expr = res.register(self.expression())
         if res.error: return res
         cases.append((condition, expr))
-
 
         while self.current_tok.matches(TOKEN_KEYWORD, 'ELIF'):
             res.register_advancement()
@@ -427,12 +441,27 @@ class Parser:
             condition = res.register(self.expression())
             if res.error: return res
 
+            if not self.current_tok.matches(TOKEN_KEYWORD, 'THEN'):
+                return res.failure(CompilerSyntaxError(self.current_tok.pos_start, self.current_tok.pos_end,
+                "Expected 'THEN' "))
             res.register_advancement()
             self.advance()
 
             expr = res.register(self.expression())
             if res.error: return res
             cases.append((condition, expr))
+
+
+
+        if self.current_tok.matches(TOKEN_KEYWORD, 'ELSE'):
+            res.register_advancement()
+            self.advance()
+
+            else_case = res.register(self.expression())
+            if res.error: return res
+
+
+        return res.success(IfNode(cases, else_case))
 
     def term(self):
        return self.bin_op(self.factor, (TOKEN_MUL, TOKEN_DIV))
@@ -575,6 +604,9 @@ class Number:
     def set_context(self, context=None):
         self.context = context
         return self
+
+    def is_true(self):
+        return self.value != 0
 
 
     #isinstance return true if two parameters are the same type or same objects    
@@ -773,6 +805,30 @@ class Interpreter:
             return res.failure(error)
         else:
             return res.success(number.set_pos(node.pos_start. node.pos_end))
+
+
+    def visit_IfNode(self, node, context):
+        res = RuntimeResult()
+
+        for condition, expr in node.cases:
+            condition_value = res.register(self.visit(condition, context))
+            if res.error: return res 
+
+            if condition_value.is_true():
+                expr_value = res.register(self.visit(expr, context))
+                if res.error: return res
+                return res.success(expr_value)
+
+        if node.else_case:
+            else_value = res.register(self.visit(node.else_case, context))
+            if res.error: return res
+            return res.success(else_value)
+
+        return res.success(None)
+
+
+
+
 
 global_symbolTable = SymbolTable()
 global_symbolTable.set("null", Number(0))
